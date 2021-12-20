b1c374e819688a00f916ddad46402bacf8c07d72
Manh <duymanh25102001@gmail.com>
2021-12-20 16:20:00 +0700

    update province

--- server/models/province.js
+++ server/models/province.js
@@ -1,4 +1,4 @@
-const sql = require("../config/pool");
+const pool = require("../config/pool");
 class Province {
     constructor(addressCode, name) {
         this.addressCode = addressCode;
@@ -6,10 +6,10 @@ class Province {
     }
     async checkIfAddressCodeExists() {
         console.log("checke1");
-        const qry = `SELECT id FROM province_a2 where id= ?`;
+        const qry = `SELECT id FROM area where id= ?`;
         try {
             console.log("checke2");
-            const [rows, fields] = await sql.query(qry, [this.addressCode]);
+            const [rows, fields] = await pool.query(qry, [this.addressCode]);
             console.log("checke3");
             if (rows.length == 1) return true;
         } catch (error) {
@@ -19,19 +19,20 @@ class Province {
         return false;
     }
     async checkIfNameExists() {
-        const qry = `SELECT name FROM province_a2 where name= ?`;
+        const qry = `SELECT name FROM area where name= ?`;
         try {
-            const [rows, fields] = await sql.query(qry, [this.name]);
+            const [rows, fields] = await pool.query(qry, [this.name]);
             if (rows.length == 1) return true;
         } catch (error) {
             console.log(error);
         }
         return false;
     }
+
     async createProvince() {
-        const qry = `INSERT INTO area(id, name) VALUES(?,?)`;
+        const qry = `INSERT INTO area(address_code, name) VALUES(?,?)`;
         try {
-            const [rows, fields] = await sql.query(qry, [
+            const [rows, fields] = await pool.query(qry, [
                 this.addressCode,
                 this.name,
             ]);
