ba4e36787db17b00cd1386b2b46804921804ed70
nhat15 <64568334+nhat15@users.noreply.github.com>
2021-12-27 12:57:01 +0700

    Update area.controller.js

--- server/controllers/area.controller.js
+++ server/controllers/area.controller.js
@@ -10,3 +10,12 @@ exports.getAreaData = async (req, res) => {
     const list = await Area.getAreaData(req.body.addressCode);
     res.json(list);
 };
+
+exports.getAreaInfor = async (req, res) => {
+    const list = await Area.getDetailArea(req.body.addressCode);
+    if (list !== null) {
+        res.json({list, status: "ok", message: "Lấy dữ liệu địa phương thành công"});
+    } else {
+        res.json({status: "error", message: "Lấy dữ liệu địa phương không thành công"});
+    }
+}
