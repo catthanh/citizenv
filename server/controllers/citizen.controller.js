e09a00bc4b32bb803adeae6e45029cbeeb0a78b0
catthanh <catthanh26@gmail.com>
2021-12-27 09:22:49 +0700

    router

--- server/controllers/citizen.controller.js
+++ server/controllers/citizen.controller.js
@@ -2,4 +2,7 @@ const Area = require("../models/area.model");
 const User = require("../models/user.model");
 const Citizen = require("../models/citizen.model");
 
-exports.getCitizenList = async (req, res) => {};
+exports.getCitizenList = async (req, res) => {
+    const citizenv = await Citizen.getCitizenListFromProvince(req.addressCode);
+    res.json(citizenv);
+};
