ead3128e696fae63f56a86a047470ad8e7aeb36e
catthanh <catthanh26@gmail.com>
2021-12-27 09:52:37 +0700

    a

--- server/controllers/citizen.controller.js
+++ server/controllers/citizen.controller.js
@@ -3,29 +3,36 @@ const User = require("../models/user.model");
 const Citizen = require("../models/citizen.model");
 
 exports.getCitizenList = async (req, res) => {
-    const citizenv = await Citizen.getCitizenListFromProvince(req.addressCode);
+    const citizenv = await Citizen.getCitizenListFromProvince(
+        req.body.addressCode
+    );
     res.json(citizenv);
 };
 
 exports.getCitizenNumber = async (req, res) => {
-    const citizenv = await Citizen.getCitizenListCateByProvince(req.addressCode);
+    const citizenv = await Citizen.getCitizenListCateByProvince(
+        req.body.addressCode
+    );
     res.json(citizenv);
 };
 
 exports.getCitizenNumberCateByGender = async (req, res) => {
-    const citizenv = await Citizen.getCitizenListCateByGender(req.addressCode);
+    const citizenv = await Citizen.getCitizenListCateByGender(
+        req.body.addressCode
+    );
     res.json(citizenv);
 };
 
 exports.getCitizenNumberCateByAge = async (req, res) => {
-    const citizenv = await Citizen.getCitizenListCateByAged(req.addressCode);
+    const citizenv = await Citizen.getCitizenListCateByAged(
+        req.body.addressCode
+    );
     res.json(citizenv);
 };
 
 exports.getCitizenNumberCateByAcademicLevel = async (req, res) => {
-    const citizenv = await Citizen.getCitizenListCateByAcademicLevel(req.addressCode);
+    const citizenv = await Citizen.getCitizenListCateByAcademicLevel(
+        req.body.addressCode
+    );
     res.json(citizenv);
 };
-
-
-
